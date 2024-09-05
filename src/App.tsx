d335ac601b717c812863c542e0703f7e1de2c77e
dominic <dominicseduraja3@gmail.com>
2024-09-06 00:26:42 +0530

    home flex added

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Link, Route} from 'react-router-dom';
 import Header from './Components/Header/Header';
+import Home from './Pages/Home/Home';
+import Support from './Pages/Support/Support';
 
 function App() {
   return (
+   <Router>
    <div className='h-screen w-screen bg-black'>
       <Header /> 
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='support' element={<Support />} />
+      </Routes>
    </div>
+   </Router>
   );  
 }
 
---
+++ src/Pages/Home/Home.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className='h-[88%] m-4 border-2 border-purple-900'>
+        <div className='flex justify-center align-center gap-10'>
+            <div className='w-[200px] h-[200px] bg-purple-400'>
+
+            </div>
+
+            <div className='w-[200px] h-[200px] bg-purple-400'>
+
+            </div>
+
+            <div className='w-[200px] h-[200px] bg-purple-400'>
+
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Home
