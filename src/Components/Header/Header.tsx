1e5508b35f26510988e9d7637deb1cd20f298218
dominic <dominicseduraja3@gmail.com>
2024-10-15 23:13:13 +0530

    add gif

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import Logo from '../../Assets/Gif/Converter.gif'
 
 type Props = {}
 
@@ -6,8 +7,9 @@ const Header = (props: Props) => {
   return (
     <div className='w-full bg-gradient-to-bl from-slate-900 via-purple-900 to-slate-900 h-[10%]'>
       <div className='h-[100%] flex justify-between items-center ml-3 mr-3'>
-        <div className='text-white'>
-            <h2>HEAD</h2>
+        <div className='text-white flex items-center'>
+            <img src={Logo} alt="logo" />
+            <h2 className='font-bold text-2xl'>Convertor</h2>
         </div>
         <div className='flex text-white gap-2'>
         <span>
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,22 +1,33 @@
 import React from 'react'
+import Image from '../../Assets/Gif/Image.gif'
+import PDF from '../../Assets/Gif/PDF.gif'
 
 type Props = {}
 
 const Home = (props: Props) => {
+
+  const data = [
+    {
+      label : 'Image to PDF'
+    },
+    {
+      label : 'Word to PDF'
+    },
+    {
+      label : 'Excell to PDF'
+    }
+  ]
   return (
     <div className='h-[88%] m-4 border-2 border-purple-900'>
-        <div className='flex justify-center align-center gap-10'>
-            <div className='w-[200px] h-[200px] bg-purple-400'>
-
-            </div>
-
-            <div className='w-[200px] h-[200px] bg-purple-400'>
-
-            </div>
-
-            <div className='w-[200px] h-[200px] bg-purple-400'>
-
-            </div>
+        <div className='flex flex-wrap justify-center align-center gap-10'>
+            {data.map((data)=>( 
+            <div className='flex-[1_0_20%] m-[25px] h-[300px] border-2 border-purple-400 rounded-md flex flex-col gap-10 items-center justify-center '>
+                <span className='text-purple-400 text-lg' >{data?.label}</span>
+                <div className='flex items-center gap-5'>
+                <img className='h-[80px] w-[80px]' src={Image} alt="gif" />
+                <img className='h-[80px] w-[80px]' src={PDF} alt="gif" />
+                </div>
+            </div>))} 
         </div>
     </div>
   )
