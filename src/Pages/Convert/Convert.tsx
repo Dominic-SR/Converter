09bc0e103a7aa360cc7b650c03a47a455bc020f4
dominic <dominicseduraja3@gmail.com>
2024-11-18 23:52:58 +0530

    renames

--- src/Pages/Convert/Convert.tsx
+++ src/Pages/Convert/Convert.tsx
@@ -11,7 +11,7 @@ console.log("PASSS",location.state);
   return (
     <div className='h-[88%] m-4 border-2 border-purple-900'>
       <div className='flex justify-center items-center h-[50%] m-10 bg-black-400 border-2 border-purple-900 border-dashed'>
-        <h1 className='text-purple-800 font-bold text-2xl'>Drag and Drop</h1>
+        <h1 className='text-purple-800 font-bold text-2xl'>Drag and Drop the files</h1>
       </div>
     </div>
   )
